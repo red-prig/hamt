1dfa0f944a15c5a63a30f9a3eaf205dca0731e06
Pavel <68122101+red-prig@users.noreply.github.com>
2023-06-06 12:16:31 +0300

    Update
    Added explicit difference between returning old value and deletion error
    Added iterators

--- hamt.pas
+++ hamt.pas
@@ -1,7 +1,8 @@
 { Simplified implementation of HAMT (Hash Array Mapped Trie) with 32bit/64bit hash key.
   Specific hash functions and collision resolution are outside the scope of
    this implementation and can be implemented on top of it.
-  Copyright (C) 2021 Red_prig
+
+  Copyright (C) 2023 Red_prig
   This library is free software; you can redistribute it and/or modify it
   under the terms of the GNU Library General Public License as published by
   the Free Software Foundation; either version 2 of the License, or (at your
@@ -37,7 +38,7 @@ function  HAMT_clear32(hamt:THAMT;cb:Tfree_data_cb;userdata:Pointer):Boolean;
 function  HAMT_destroy32(hamt:THAMT;cb:Tfree_data_cb;userdata:Pointer):Boolean;
 function  HAMT_search32(hamt:THAMT;key:DWORD):PPointer;              //mutable link to data
 function  HAMT_insert32(hamt:THAMT;key:DWORD;data:Pointer):PPointer; //mutable link to data
-function  HAMT_delete32(hamt:THAMT;key:DWORD):Pointer;               //data
+function  HAMT_delete32(hamt:THAMT;key:DWORD;old:PPointer):Boolean;  //data
 function  HAMT_traverse32(hamt:THAMT;cb:Tfree_data_cb;userdata:Pointer):Boolean;
 
 function  HAMT_create64:THAMT;
@@ -45,7 +46,7 @@ function  HAMT_clear64(hamt:THAMT;cb:Tfree_data_cb;userdata:Pointer):Boolean;
 function  HAMT_destroy64(hamt:THAMT;cb:Tfree_data_cb;userdata:Pointer):Boolean;
 function  HAMT_search64(hamt:THAMT;key:QWORD):PPointer;              //mutable link to data
 function  HAMT_insert64(hamt:THAMT;key:QWORD;data:Pointer):PPointer; //mutable link to data
-function  HAMT_delete64(hamt:THAMT;key:QWORD):Pointer;               //data
+function  HAMT_delete64(hamt:THAMT;key:QWORD;old:PPointer):Boolean;  //data
 function  HAMT_traverse64(hamt:THAMT;cb:Tfree_data_cb;userdata:Pointer):Boolean;
 
 type
@@ -96,6 +97,52 @@ type
 
  TSTUB_HAMT32=array[0..HAMT32.root_size-1] of THAMTNode32;
  TSTUB_HAMT64=array[0..HAMT64.root_size-1] of THAMTNode64;
+
+ //iterators
+
+ PHAMT_Iterator32=^THAMT_Iterator32;
+ THAMT_Iterator32=packed object
+  type
+   PStackNode=^TStackNode;
+   TStackNode=packed record
+    bnode,cnode,enode:PHAMTNode32;
+   end;
+  var
+   cpos:Ptruint;
+   data:array[0..HAMT32.stack_max+1] of TStackNode;
+ end;
+
+ PHAMT_Iterator64=^THAMT_Iterator64;
+ THAMT_Iterator64=packed object
+  type
+   PStackNode=^TStackNode;
+   TStackNode=packed record
+    bnode,cnode,enode:PHAMTNode64;
+   end;
+  var
+   cpos:Ptruint;
+   data:array[0..HAMT64.stack_max+1] of TStackNode;
+ end;
+
+//32
+
+function HAMT_first32(hamt:THAMT;i:PHAMT_Iterator32):Boolean;
+function HAMT_last32(hamt:THAMT;i:PHAMT_Iterator32):Boolean;
+
+function HAMT_next32(i:PHAMT_Iterator32):Boolean;
+function HAMT_prev32(i:PHAMT_Iterator32):Boolean;
+
+function HAMT_get_value32(i:PHAMT_Iterator32;v:PPointer):Boolean;
+
+//64
+
+function HAMT_first64(hamt:THAMT;i:PHAMT_Iterator64):Boolean;
+function HAMT_last64(hamt:THAMT;i:PHAMT_Iterator64):Boolean;
+
+function HAMT_next64(i:PHAMT_Iterator64):Boolean;
+function HAMT_prev64(i:PHAMT_Iterator64):Boolean;
+
+function HAMT_get_value64(i:PHAMT_Iterator64;v:PPointer):Boolean;
 
 implementation
 
@@ -376,6 +423,7 @@ var
  data:array[0..HAMT32.stack_max] of TStackNode;
  Size:QWORD;
 begin
+ if (node=nil) or (cb=nil) then Exit;
  if IsSubTrie32(node) then
  begin
   curr:=@data;
@@ -405,15 +453,13 @@ begin
     end;
    end else
    begin
-    if (cb<>nil) then
-     cb(GetValue32(curr^.cnode),userdata);
+    cb(GetValue32(curr^.cnode),userdata);
     Inc(curr^.cnode);
    end;
   until false;
  end else
  begin
-  if (cb<>nil) then
-   cb(GetValue32(node),userdata);
+  cb(GetValue32(node),userdata);
  end;
 end;
 
@@ -428,6 +474,7 @@ var
  data:array[0..HAMT64.stack_max] of TStackNode;
  Size:QWORD;
 begin
+ if (node=nil) or (cb=nil) then Exit;
  if IsSubTrie64(node) then
  begin
   curr:=@data;
@@ -457,15 +504,13 @@ begin
     end;
    end else
    begin
-    if (cb<>nil) then
-     cb(GetValue64(curr^.cnode),userdata);
+    cb(GetValue64(curr^.cnode),userdata);
     Inc(curr^.cnode);
    end;
   until false;
  end else
  begin
-  if (cb<>nil) then
-   cb(GetValue64(node),userdata);
+  cb(GetValue64(node),userdata);
  end;
 end;
 
@@ -825,13 +870,14 @@ begin
  until false;
 end;
 
-function HAMT_delete32(hamt:THAMT;key:DWORD):Pointer;
+function HAMT_delete32(hamt:THAMT;key:DWORD;old:PPointer):Boolean;
 var
  prev,node,oldnodes,newnodes:PHAMTNode32;
  keypart,Map,Size:DWORD;
  keypartbits:DWORD;
 begin
- if (hamt=nil) then Exit(nil);
+ Result:=False;
+ if (hamt=nil) then Exit;
 
  keypartbits:=HAMT32.root_bits;
 
@@ -841,14 +887,18 @@ begin
  keypart:=key and HAMT32.root_mask;
  node:=@PHAMTNode32(hamt)[keypart];
 
- if (node^.BaseValue=nil) then Exit(nil);
+ if (node^.BaseValue=nil) then Exit;
 
  repeat
   if not IsSubTrie32(node) then
   begin
    if (node^.BitMapKey=key) then
    begin
-    Result:=GetValue32(node);
+    Result:=True;
+    if (old<>nil) then
+    begin
+     old^:=GetValue32(node);
+    end;
 
     node^:=Default(THAMTNode32);
 
@@ -885,7 +935,7 @@ begin
 
     Exit;
    end else
-    Exit(nil);
+    Exit;
   end;
   //Subtree: look up in bitmap
   Assert(keypartbits<HAMT32.node_size);
@@ -893,7 +943,7 @@ begin
   keypart:=(key shr keypartbits) and HAMT32.node_mask;
 
   if BitIsNotSet32(node^.BitMapKey,keypart) then
-   Exit(nil); // bit is 0 in bitmap -> no match
+   Exit; // bit is 0 in bitmap -> no match
 
   Map:=GetMapPos32(node^.BitMapKey,keypart);
 
@@ -905,13 +955,14 @@ begin
  until false;
 end;
 
-function HAMT_delete64(hamt:THAMT;key:QWORD):Pointer;
+function HAMT_delete64(hamt:THAMT;key:QWORD;old:PPointer):Boolean;
 var
  prev,node,oldnodes,newnodes:PHAMTNode64;
  keypart,Map,Size:QWORD;
  keypartbits:QWORD;
 begin
- if (hamt=nil) then Exit(nil);
+ Result:=False;
+ if (hamt=nil) then Exit;
 
  keypartbits:=HAMT64.root_bits;
 
@@ -921,14 +972,18 @@ begin
  keypart:=key and HAMT64.root_mask;
  node:=@PHAMTNode64(hamt)[keypart];
 
- if (node^.BaseValue=nil) then Exit(nil);
+ if (node^.BaseValue=nil) then Exit;
 
  repeat
   if not IsSubTrie64(node) then
   begin
    if (node^.BitMapKey=key) then
    begin
-    Result:=GetValue64(node);
+    Result:=True;
+    if (old<>nil) then
+    begin
+     old^:=GetValue64(node);
+    end;
 
     node^:=Default(THAMTNode64);
 
@@ -965,7 +1020,7 @@ begin
 
     Exit;
    end else
-    Exit(nil);
+    Exit;
   end;
   //Subtree: look up in bitmap
   Assert(keypartbits<HAMT64.node_size);
@@ -973,7 +1028,7 @@ begin
   keypart:=(key shr keypartbits) and HAMT64.node_mask;
 
   if BitIsNotSet64(node^.BitMapKey,keypart) then
-   Exit(nil); // bit is 0 in bitmap -> no match
+   Exit; // bit is 0 in bitmap -> no match
 
   Map:=GetMapPos64(node^.BitMapKey,keypart);
 
@@ -984,6 +1039,365 @@ begin
   keypartbits:=keypartbits+HAMT64.node_bits;
  until false;
 end;
+
+//iterator32
+
+function _HAMT_next32(i:PHAMT_Iterator32):Boolean;
+var
+ curr:THAMT_Iterator32.PStackNode;
+ node:PHAMTNode32;
+ Size:QWORD;
+begin
+ Result:=False;
+
+ curr:=@i^.data[i^.cpos];
+
+ repeat
+  if (curr^.cnode>=curr^.enode) then
+  begin
+   //up
+   if (i^.cpos=0) then Exit;
+   Dec(i^.cpos);
+   Dec(curr);
+   //next
+   Inc(curr^.cnode);
+   Continue;
+  end;
+  if IsSubTrie32(curr^.cnode) then
+  begin
+   //down
+   node:=curr^.cnode;
+   Inc(i^.cpos);
+   Inc(curr);
+   Size:=GetBitMapSize32(node^.BitMapKey);
+   With curr^ do
+   begin
+    //new
+    bnode:=GetSubTrie32(node);
+    cnode:=bnode;
+    enode:=@bnode[Size];
+   end;
+  end else
+  begin
+   Exit(True);
+  end;
+ until false;
+end;
+
+//
+
+function _HAMT_prev32(i:PHAMT_Iterator32):Boolean;
+var
+ curr:THAMT_Iterator32.PStackNode;
+ node:PHAMTNode32;
+ Size:QWORD;
+begin
+ Result:=False;
+
+ curr:=@i^.data[i^.cpos];
+
+ repeat
+  if (curr^.cnode<curr^.bnode) then
+  begin
+   //up
+   if (i^.cpos=0) then Exit;
+   Dec(i^.cpos);
+   Dec(curr);
+   //prev
+   Dec(curr^.cnode);
+   Continue;
+  end;
+  if IsSubTrie32(curr^.cnode) then
+  begin
+   //down
+   node:=curr^.cnode;
+   Inc(i^.cpos);
+   Inc(curr);
+   Size:=GetBitMapSize32(node^.BitMapKey);
+   With curr^ do
+   begin
+    //new
+    bnode:=GetSubTrie32(node);
+    enode:=@bnode[Size];
+    cnode:=enode;
+    Dec(cnode);
+   end;
+  end else
+  begin
+   Exit(True);
+  end;
+ until false;
+end;
+
+
+function HAMT_first32(hamt:THAMT;i:PHAMT_Iterator32):Boolean;
+var
+ node:THAMT_Iterator32.TStackNode;
+begin
+ if (hamt=nil) then Exit(False);
+
+ node.bnode:=@PHAMTNode32(hamt)[0];
+ node.cnode:=node.bnode;
+ node.enode:=@node.bnode[HAMT32.root_mask+1];
+
+ i^:=Default(THAMT_Iterator32);
+ i^.data[0]:=node;
+
+ Result:=_HAMT_next32(i);
+end;
+
+function HAMT_last32(hamt:THAMT;i:PHAMT_Iterator32):Boolean;
+var
+ node:THAMT_Iterator32.TStackNode;
+begin
+ if (hamt=nil) then Exit(False);
+
+ node.bnode:=@PHAMTNode32(hamt)[0];
+ node.enode:=@node.bnode[HAMT32.root_mask+1];
+ node.cnode:=node.enode;
+ Dec(node.cnode);
+
+ i^:=Default(THAMT_Iterator32);
+ i^.data[0]:=node;
+
+ Result:=_HAMT_prev32(i);
+end;
+
+function HAMT_next32(i:PHAMT_Iterator32):Boolean;
+var
+ curr:THAMT_Iterator32.PStackNode;
+begin
+ Result:=False;
+ if (i=nil) then Exit;
+ if (i^.cpos>=Length(i^.data)) then Exit;
+
+ curr:=@i^.data[i^.cpos];
+
+ if (curr^.cnode<curr^.enode) then
+ begin
+  //next
+  Inc(curr^.cnode);
+ end;
+
+ Result:=_HAMT_next32(i);
+end;
+
+
+function HAMT_prev32(i:PHAMT_Iterator32):Boolean;
+var
+ curr:THAMT_Iterator32.PStackNode;
+begin
+ Result:=False;
+ if (i=nil) then Exit;
+ if (i^.cpos>=Length(i^.data)) then Exit;
+
+ curr:=@i^.data[i^.cpos];
+
+ if (curr^.cnode>=curr^.bnode) then
+ begin
+  //prev
+  Dec(curr^.cnode);
+ end;
+
+ Result:=_HAMT_prev32(i);
+end;
+
+function HAMT_get_value32(i:PHAMT_Iterator32;v:PPointer):Boolean;
+var
+ curr:THAMT_Iterator32.PStackNode;
+begin
+ Result:=False;
+ if (i=nil) or (v=nil) then Exit;
+ if (i^.cpos>=Length(i^.data)) then Exit;
+
+ curr:=@i^.data[i^.cpos];
+
+ if IsSubTrie32(curr^.cnode) then Exit;
+
+ v^:=GetValue32(curr^.cnode);
+ Result:=True;
+end;
+
+//iterator64
+
+function _HAMT_next64(i:PHAMT_Iterator64):Boolean;
+var
+ curr:THAMT_Iterator64.PStackNode;
+ node:PHAMTNode64;
+ Size:QWORD;
+begin
+ Result:=False;
+
+ curr:=@i^.data[i^.cpos];
+
+ repeat
+  if (curr^.cnode>=curr^.enode) then
+  begin
+   //up
+   if (i^.cpos=0) then Exit;
+   Dec(i^.cpos);
+   Dec(curr);
+   //next
+   Inc(curr^.cnode);
+   Continue;
+  end;
+  if IsSubTrie64(curr^.cnode) then
+  begin
+   //down
+   node:=curr^.cnode;
+   Inc(i^.cpos);
+   Inc(curr);
+   Size:=GetBitMapSize64(node^.BitMapKey);
+   With curr^ do
+   begin
+    //new
+    bnode:=GetSubTrie64(node);
+    cnode:=bnode;
+    enode:=@bnode[Size];
+   end;
+  end else
+  begin
+   Exit(True);
+  end;
+ until false;
+end;
+
+//
+
+function _HAMT_prev64(i:PHAMT_Iterator64):Boolean;
+var
+ curr:THAMT_Iterator64.PStackNode;
+ node:PHAMTNode64;
+ Size:QWORD;
+begin
+ Result:=False;
+
+ curr:=@i^.data[i^.cpos];
+
+ repeat
+  if (curr^.cnode<curr^.bnode) then
+  begin
+   //up
+   if (i^.cpos=0) then Exit;
+   Dec(i^.cpos);
+   Dec(curr);
+   //prev
+   Dec(curr^.cnode);
+   Continue;
+  end;
+  if IsSubTrie64(curr^.cnode) then
+  begin
+   //down
+   node:=curr^.cnode;
+   Inc(i^.cpos);
+   Inc(curr);
+   Size:=GetBitMapSize64(node^.BitMapKey);
+   With curr^ do
+   begin
+    //new
+    bnode:=GetSubTrie64(node);
+    enode:=@bnode[Size];
+    cnode:=enode;
+    Dec(cnode);
+   end;
+  end else
+  begin
+   Exit(True);
+  end;
+ until false;
+end;
+
+
+function HAMT_first64(hamt:THAMT;i:PHAMT_Iterator64):Boolean;
+var
+ node:THAMT_Iterator64.TStackNode;
+begin
+ if (hamt=nil) then Exit(False);
+
+ node.bnode:=@PHAMTNode64(hamt)[0];
+ node.cnode:=node.bnode;
+ node.enode:=@node.bnode[HAMT64.root_mask+1];
+
+ i^:=Default(THAMT_Iterator64);
+ i^.data[0]:=node;
+
+ Result:=_HAMT_next64(i);
+end;
+
+function HAMT_last64(hamt:THAMT;i:PHAMT_Iterator64):Boolean;
+var
+ node:THAMT_Iterator64.TStackNode;
+begin
+ if (hamt=nil) then Exit(False);
+
+ node.bnode:=@PHAMTNode64(hamt)[0];
+ node.enode:=@node.bnode[HAMT64.root_mask+1];
+ node.cnode:=node.enode;
+ Dec(node.cnode);
+
+ i^:=Default(THAMT_Iterator64);
+ i^.data[0]:=node;
+
+ Result:=_HAMT_prev64(i);
+end;
+
+function HAMT_next64(i:PHAMT_Iterator64):Boolean;
+var
+ curr:THAMT_Iterator64.PStackNode;
+begin
+ Result:=False;
+ if (i=nil) then Exit;
+ if (i^.cpos>=Length(i^.data)) then Exit;
+
+ curr:=@i^.data[i^.cpos];
+
+ if (curr^.cnode<curr^.enode) then
+ begin
+  //next
+  Inc(curr^.cnode);
+ end;
+
+ Result:=_HAMT_next64(i);
+end;
+
+
+function HAMT_prev64(i:PHAMT_Iterator64):Boolean;
+var
+ curr:THAMT_Iterator64.PStackNode;
+begin
+ Result:=False;
+ if (i=nil) then Exit;
+ if (i^.cpos>=Length(i^.data)) then Exit;
+
+ curr:=@i^.data[i^.cpos];
+
+ if (curr^.cnode>=curr^.bnode) then
+ begin
+  //prev
+  Dec(curr^.cnode);
+ end;
+
+ Result:=_HAMT_prev64(i);
+end;
+
+function HAMT_get_value64(i:PHAMT_Iterator64;v:PPointer):Boolean;
+var
+ curr:THAMT_Iterator64.PStackNode;
+begin
+ Result:=False;
+ if (i=nil) or (v=nil) then Exit;
+ if (i^.cpos>=Length(i^.data)) then Exit;
+
+ curr:=@i^.data[i^.cpos];
+
+ if IsSubTrie64(curr^.cnode) then Exit;
+
+ v^:=GetValue64(curr^.cnode);
+ Result:=True;
+end;
+
+//
+
 
 end.
 
